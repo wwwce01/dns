d7b0f6b479971d7ef0ea6e0ae8698945c2f7d148
wwwce01 <95126799+wwwce01@users.noreply.github.com>
2023-08-30 17:07:09 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -11,6 +11,13 @@ domainID=a81fe9795838a65e8982aa73978b13a7
 //这个是具体解析域名的ID 获取 方式请看教程 ce.2wr.life
 dnsID=3e8e36f7ffb823a97fd99b2d60c2f79f
 
+resultIP=$(curl -X POST "https://api.hostmonit.com/get_optimization_ip" \
+-H "Referer: https://stock.hostmonit.com/" \
+-H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36" \
+-H "Content-Type: application/json" \
+--data '{"key":"iDetkOys"}')
+
+echo $resultIP
 curl -X PUT "https://api.cloudflare.com/client/v4/zones/$domainID/dns_records/$dnsID" \
 -H "X-Auth-Email: $gmail" \
 -H "X-Auth-Key: $key" \
